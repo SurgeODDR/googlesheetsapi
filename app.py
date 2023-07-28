bbfb89253d1776277831a7d05226957e374c1f2e
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-07-28 13:53:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
-from flask import Flask, jsonify
-import logging
+from flask import Flask, jsonify, current_app as app
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
-import json  # add this line
-import sys  # add this line for stdout logging
+import json
 
-# Set up Flask
 app = Flask(__name__)
-
-# Set up logging to stdout
-handler = logging.StreamHandler(sys.stdout)  # log to stdout
-handler.setLevel(logging.INFO)  # log INFO level and above
-app.logger.addHandler(handler)  # attach the handler to the app's logger
 
 @app.route('/format_sheets', methods=['GET'])
 def format_sheets():
     try:
         app.logger.info("Fetching the Google service account credentials from Azure Key Vault...")
-
-        # Fetch the Google service account credentials from Azure Key Vault
         vault_url = "https://keyvaultxscrapingoddr.vault.azure.net/"
         credential = DefaultAzureCredential()
         secret_client = SecretClient(vault_url=vault_url, credential=credential)
@@ -28,44 +18,34 @@ def format_sheets():
         credentials_json_str = secret_client.get_secret(secret_name).value
 
         app.logger.info(f"Fetched credentials: {credentials_json_str}")
-
-        # Parse the JSON string into a Python dictionary
         credentials_dict = json.loads(credentials_json_str)
 
         app.logger.info("Setting up Google Sheets API client...")
-
-        # Set up Google Sheets API client
-        scope = ['https://spreadsheets.google.com/feeds',
-                 'https://www.googleapis.com/auth/drive']
+        scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
         client = gspread.authorize(credentials)
 
         app.logger.info("Opening the spreadsheet...")
-
-        # Open the spreadsheet by title
         spreadsheet = client.open("twitch_data")
 
         app.logger.info("Formatting all sheets...")
-
-        # Format all sheets
         for worksheet in spreadsheet.worksheets():
-            app.logger.info(f"Processing worksheet: {worksheet.title}")  # log the worksheet title
-            cell_list = worksheet.range('A1:Z1000')  # Adjust the range if you expect more data
+            app.logger.info(f"Processing worksheet: {worksheet.title}")
+            cell_list = worksheet.range('A1:Z1000')
             for cell in cell_list:
-                cell.background = "#00FF00"  # lime green
+                cell.background = "#00FF00"
             worksheet.update_cells(cell_list)
-            app.logger.info(f"Updated {len(cell_list)} cells in worksheet: {worksheet.title}")  # log the number of updated cells
+            app.logger.info(f"Updated {len(cell_list)} cells in worksheet: {worksheet.title}")
 
         return jsonify(success=True)
 
     except Exception as e:
-        app.logger.exception(e)  # Log the exception
+        app.logger.exception(e)
         return jsonify(error=str(e)), 500
 
-# Define error handler for 500 status code
 @app.errorhandler(500)
 def server_error(e):
-    app.logger.exception(e)  # Log the exception
+    app.logger.exception(e)
     return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
