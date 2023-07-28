1319a12a9d55f11457d4c8debeb5fdf1300f841e
SurgeODDR <140343038+SurgeODDR@users.noreply.github.com>
2023-07-28 13:22:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,55 @@
-from flask import Flask
-import gspread
+from flask import Flask, jsonify
+import logging
 from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 
+# Set up Flask
 app = Flask(__name__)
 
-@app.route('/')
+# Set up logging
+handler = logging.FileHandler('app.log')  # log to a file
+handler.setLevel(logging.INFO)  # log INFO level and above
+app.logger.addHandler(handler)  # attach the handler to the app's logger
+
+@app.route('/format_sheets', methods=['GET'])
 def format_sheets():
-    # Fetch credentials from Azure Key Vault
-    key_vault_name = 'Keyvaultxscrapingoddr'
-    KVUri = f"https://{key_vault_name}.vault.azure.net"
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=KVUri, credential=credential)
-    secret_value = client.get_secret("YT-Scraper-web-googleservicekey")
+    try:
+        # Fetch the Google service account credentials from Azure Key Vault
+        vault_url = "https://keyvaultxscrapingoddr.vault.azure.net/"
+        credential = DefaultAzureCredential()
+        secret_client = SecretClient(vault_url=vault_url, credential=credential)
+        secret_name = "YT-Scraper-web-googleservicekey"
+        credentials_json = secret_client.get_secret(secret_name).value
 
-    # Use creds to create a client to interact with the Google Drive API
-    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(secret_value.value, scope)
-    client = gspread.authorize(creds)
+        # Set up Google Sheets API client
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
+        client = gspread.authorize(credentials)
 
-    # Open the Google Spreadsheet using its name
-    spreadsheet = client.open('twitch_data')  # Update with your Spreadsheet Name
+        # Open the spreadsheet by title
+        spreadsheet = client.open("twitch_data")
 
-    # Fetch all worksheets
-    worksheets = spreadsheet.worksheets()
+        # Format all sheets
+        for worksheet in spreadsheet.worksheets():
+            cell_list = worksheet.range('A1:Z1000')  # Adjust the range if you expect more data
+            for cell in cell_list:
+                cell.background = "#00FF00"  # lime green
+            worksheet.update_cells(cell_list)
 
-    # Loop through each worksheet
-    for worksheet in worksheets:
-        # Set the font size and make the header row bold
-        cell_format = gspread.format.CellFormat(fontSize=10, textFormat=gspread.format.TextFormat(bold=True))
-        worksheet.format("1:1", cell_format)
+        return jsonify(success=True)
 
-        # Freeze the header row
-        worksheet.frozen_rows = 1
+    except Exception as e:
+        app.logger.exception(e)  # Log the exception
+        return jsonify(error=str(e)), 500
 
-        # Additional formatting for 'geography' and 'demographics' sheets
-        if worksheet.title == 'geography' or worksheet.title == 'demographics':
-            # Assuming the username is in the first column
-            usernames = worksheet.col_values(1)
-            data = worksheet.get_all_values()
-
-            # Move the username column to the last position
-            for i, row in enumerate(data):
-                row.append(row.pop(0))
-                worksheet.insert_row(row, i+1)
-
-            # Delete the original username column
-            worksheet.delete_columns(1)
-
-    return "Formatted the sheets successfully", 200
+# Define error handler for 500 status code
+@app.errorhandler(500)
+def server_error(e):
+    app.logger.exception(e)  # Log the exception
+    return jsonify(error=str(e)), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
